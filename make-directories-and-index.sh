5efc8bf99ebcfe82d3a6d0757e24fa1eb029bffc
SebastJava <sebastjava@hotmail.ca>
2021-01-19 18:22:19 -0500

    Moved and replaced files from UPSTREAM-UPDATE directory into appropriate into main and sub-directories, by hand
    	new file:   My-complete-step-by-step.txt
    	modified:   README.md
    	new file:   UPSTREAM-UPDATE (copy)/My-complete-step-by-step.txt
    	new file:   UPSTREAM-UPDATE (copy)/README.md
    	new file:   UPSTREAM-UPDATE (copy)/UPSTREAM-UPDATE-files
    	new file:   UPSTREAM-UPDATE (copy)/Z-NEW.md
    	new file:   UPSTREAM-UPDATE (copy)/colors-preview.png
    	new file:   UPSTREAM-UPDATE (copy)/colors-preview.svg
    	new file:   UPSTREAM-UPDATE (copy)/debian/changelog
    	new file:   UPSTREAM-UPDATE (copy)/debian/control
    	new file:   UPSTREAM-UPDATE (copy)/debian/copyright
    	new file:   UPSTREAM-UPDATE (copy)/debian/postinst
    	new file:   UPSTREAM-UPDATE (copy)/generate-color-variations.py
    	new file:   UPSTREAM-UPDATE (copy)/index.theme
    	new file:   UPSTREAM-UPDATE (copy)/make-directories-and-index.sh
    	new file:   UPSTREAM-UPDATE (copy)/places/generate-color-variations.py
    	new file:   UPSTREAM-UPDATE (copy)/previews/nemo-preview-Blue-Yellow.png
    	new file:   UPSTREAM-UPDATE (copy)/previews/nemo-preview-MintSoda-Aqua.png
    	new file:   UPSTREAM-UPDATE (copy)/previews/nemo-preview-Orange-Blue.png
    	new file:   UPSTREAM-UPDATE (copy)/previews/nemo-preview-PurpleDeep-Pink.png
    	new file:   UPSTREAM-UPDATE (copy)/render_places.py
    	new file:   UPSTREAM-UPDATE (copy)/usr|share|icons|Mint-Yz-Old-Dark/index.theme
    	new file:   UPSTREAM-UPDATE (copy)/usr|share|icons|Mint-Yz-Old/index.theme
    	new file:   UPSTREAM-UPDATE-files
    	new file:   Z-NEW.md
    	new file:   colors-preview.png
    	new file:   colors-preview.svg
    	modified:   debian/changelog
    	modified:   debian/control
    	modified:   debian/copyright
    	modified:   debian/postinst
    	new file:   index.theme
    	new file:   make-directories-and-index.sh
    	new file:   previews/nemo-preview-Blue-Yellow.png
    	new file:   previews/nemo-preview-MintSoda-Aqua.png
    	new file:   previews/nemo-preview-Orange-Blue.png
    	new file:   previews/nemo-preview-PurpleDeep-Pink.png
    	modified:   src/places/generate-color-variations.py
    	modified:   src/render_places.py
    	modified:   usr/share/icons/Mint-Y-Dark/index.theme
    	modified:   usr/share/icons/Mint-Y/index.theme

---
+++ make-directories-and-index.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+names=("Mint-Yz-Aqua" "Mint-Yz-Blue" "Mint-Yz-BlueDeep" "Mint-Yz-Grey" "Mint-Yz-MintSoda" "Mint-Yz-MintSoft" "Mint-Yz-Orange" "Mint-Yz-Pink" "Mint-Yz-Purple" "Mint-Yz-PurpleDeep" "Mint-Yz-Red" "Mint-Yz-Yellow")
+maindir=$PWD
+
+# Make directories and index.theme for all colors-light (except Mint-Yz-Old)
+for name in "${names[@]}"; do
+    mkdir -p "usr/share/icons/$name"
+    cp -f index.theme "usr/share/icons/$name/"
+    cd "usr/share/icons/$name"
+    sed -i "s/Name=/Name=$name/" index.theme
+    cd "$maindir"
+done
+
+# Make directories and index.theme for all colors-dark (except Mint-Yz-Old-Dark)
+# Add symbolic link to Mint-Yz-COLOR/places/ (light variant)
+for name in "${names[@]}"; do
+    mkdir -p "usr/share/icons/$name-Dark"
+    cp -f index.theme "usr/share/icons/$name-Dark/"
+    cd "usr/share/icons/$name-Dark"
+    sed -i "s/Name=/Name=$name-Dark/" index.theme
+    sed -i "s/Inherits=Mint-Yz-Old,Adwaita,gnome,hicolor/Inherits=Mint-Yz-Old-Dark,Adwaita,gnome,hicolor/" index.theme
+    ln -sf "../$name/places"
+    cd "$maindir"
+done
